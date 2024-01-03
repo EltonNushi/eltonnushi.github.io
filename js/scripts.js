618c650d98d5fea6a8b7a76e63f9e20f5047ca0a
EltonNushi <elton_nushi@yahoo.co.uk>
2024-01-03 23:54:39 +0000

    Updated2

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 */
 //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -17,6 +17,33 @@ window.addEventListener('DOMContentLoaded', event => {
             offset: 74,
         });
     };
+
+window.open(page,“popup2”,“width=500, height=200”);
+
+function window.external.ShowPopup(Name, URL, Width, Height, Top, Left, Param);
+
+    procedure ShowPopup(const Name, URL: String; Width, Height,
+     Top, Left: Integer; IsModal, RedirectLinksToMain: Boolean);
+
+    procedure ShowFirstPopup;
+  begin
+   ShowPopup("mypopup", "popup1.htm", 400, 300, 50, 25, false, false);
+  end;
+
+    { NewWindow: opens a new popup window. URL : url to the page to display.
+WindowName: name of the popup (for targets and other functions).
+width: width of the popup height: height of the popup top:
+y screen position of the popup left: x screen position of the popup.
+redirect: if "1", then all links are redirected to the main window. }
+
+procedure NewWindow(Url, WindowName, Width, Height, Top, Left, Redirect: String);
+begin
+ShowPopup(WindowName, Url, StrToInt(width), StrToInt(height),
+StrToInt(top),StrToInt(left), false, Redirect = "1");
+end;
+
+
+
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
